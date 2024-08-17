6c380869568accf4d6f7b8d468f19055fac57702
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2024-08-17 18:54:19 +0530

    Time: 1076 ms (5.82%), Space: 9.1 MB (57.97%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,16 +1,14 @@
 int singleNumber(int* nums, int numsSize) {
-    int num;
-    for(int i=0;i<numsSize;i++){
+    int i;
+    for(i=0;i<numsSize;i++){
         int count=0;
         for(int j=0;j<numsSize;j++){
             if(nums[i]==nums[j]){
                 count++;
             }
         }
-        if(count==1){
-          num=nums[i];   
+        if(count==1)  
             break;
-        }
     }
-    return num;
+    return nums[i];
 }

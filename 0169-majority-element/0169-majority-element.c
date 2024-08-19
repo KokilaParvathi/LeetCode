ce9c936741c2e8e063d7c07c6c69a79eff78fa9c
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2024-08-19 07:05:32 +0530

    Time: 13 ms (96.58%), Space: 9.9 MB (65.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,17 +1,13 @@
 int majorityElement(int* nums, int numsSize) {
     int count = 0;
-    int candidate = 0;
+    int majority = 0;
 
     for (int i = 0; i < numsSize; i++) {
         if (count == 0) {
-            candidate = nums[i];
+            majority = nums[i];
         }
-        if (nums[i] == candidate) {
-            count++;
-        } else {
-            count--;
-        }
+        count += (nums[i] == majority) ? 1 : -1;
     }
 
-    return candidate;
+    return majority;
 }

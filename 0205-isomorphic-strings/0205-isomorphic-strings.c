d45b9e807f0087b17bebb4da712ef4bd4f295ab2
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2024-08-21 07:27:37 +0530

    Time: 1543 ms (5.02%), Space: 8.2 MB (30.75%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.c
@@ -0,0 +1,31 @@
+bool isIsomorphic(char* s, char* t) {
+    int len1=strlen(s);
+    int len2=strlen(t);
+    short c=1;
+    if (len1 != len2) {
+        return false;  
+    }
+
+   for (int i = 0; i < len1; i++) {
+        for (int j = i + 1; j < len2; j++) {
+            if (s[i] == s[j]) {
+                if (t[i] != t[j]) {
+                    c = 0;
+                    break;
+                }
+            } else if (t[i] == t[j]) {
+                c = 0;  
+                break;
+            }
+        }
+        if (c == 0) {
+            break;
+        }
+    }
+    
+    if (c == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}

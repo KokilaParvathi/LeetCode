dcb1352f43db6ebe3969a773fef5ef7ee27bb15d
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2024-08-27 18:46:23 +0530

    Time: 753 ms (12.81%), Space: 9.5 MB (43.22%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.c
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -1,8 +1,7 @@
 int pivotIndex(int* nums, int numsSize) {
-int i,l,r,flag=0;
-    for( i=0;i<numsSize;i++){
-        l=0;
-        r=0;
+    for( int i=0;i<numsSize;i++){
+        int l=0;
+        int r=0;
         for(int j=0;j<i;j++){
             l=l+nums[j];
         }

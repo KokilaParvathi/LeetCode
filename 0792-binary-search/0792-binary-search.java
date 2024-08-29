859d091963a85e7c5cf56eb33d3b6a9dc7bc1775
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2024-08-29 18:21:36 +0530

    Time: 0 ms (100%), Space: 45.7 MB (32.49%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+    int right=nums.length-1;
+    int left=0;
+    while(right>=left){
+        int mid=(right+left)/2;
+        if(nums[mid]==target)
+        return mid;
+        else if(nums[mid]<target)
+        left=mid+1;
+        else
+        right=mid-1;
+    } 
+    return -1;
+    }
+}

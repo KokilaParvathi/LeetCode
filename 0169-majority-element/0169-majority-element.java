6312740f7cc77f96df2d0867dfa78fa1d874252a
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-01-04 15:12:25 +0530

    Time: 2 ms (76.71%), Space: 53 MB (51.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            throw new IllegalArgumentException("No majority element found");
+        }
+    }
+}

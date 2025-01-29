0fdc8340ccc98ad41971cba902684eabcf7c2da5
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-01-29 16:13:13 +0530

    Time: 7 ms (5.05%), Space: 55 MB (99.67%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        int d= k%nums.length;
+        if(nums.length==1) return;
+       for(int itr = nums.length-d;itr<nums.length;itr++){
+        queue.add(nums[itr]);
+       }
+       for(int itr =0;itr<nums.length-d;itr++){
+        queue.add(nums[itr]);
+       }
+       int itr=0;;
+       while(!queue.isEmpty()){
+        nums[itr++]=queue.poll();
+       }
+    }
+}

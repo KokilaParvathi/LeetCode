28b9434464da9e0ee38ac847e1e5b091a02ce297
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-01 19:03:23 +0530

    Time: 15 ms (20.1%), Space: 56.5 MB (86.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,8 +2,8 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
         int smallpositive=1;
-        for(int index=0;index<nums.length;index++ ){
-            if(nums[index]==smallpositive) smallpositive++;
+        for(int n:nums){
+            if(n==smallpositive) smallpositive++;
             }
         return smallpositive;
     }

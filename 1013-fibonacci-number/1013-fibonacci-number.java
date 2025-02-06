e83d22f00213a1a2f713e47a3da4ff4c1e5423fd
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-06 22:04:22 +0530

    Time: 9 ms (25.13%), Space: 40.2 MB (78.82%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fibi(int n){
+        if(n<=1)
+        return n;
+    return fibi(n-1)+fibi(n-2);
+    }
+    public int fib(int n) {
+     int res = fibi(n);
+     return res;   
+    }
+    
+}

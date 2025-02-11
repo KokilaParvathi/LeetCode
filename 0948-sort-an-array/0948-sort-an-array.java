d3c3dd6fdff3b3352c8009a19e9b9017c0ae8d28
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-11 11:43:26 +0530

    Time: 30 ms (38.63%), Space: 56.7 MB (31.1%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,6 +1,37 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-     Arrays.sort(nums);
-     return nums;   
+        if (nums == null || nums.length <= 1) return nums;
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) leftArr[i] = nums[left + i];
+        for (int i = 0; i < n2; i++) rightArr[i] = nums[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) nums[k++] = leftArr[i++];
+            else nums[k++] = rightArr[j++];
+        }
+
+        while (i < n1) nums[k++] = leftArr[i++];
+        while (j < n2) nums[k++] = rightArr[j++];
     }
 }

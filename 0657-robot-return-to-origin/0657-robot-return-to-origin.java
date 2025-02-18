e53462e3494857ea30a14bc80c1d8ecb0ac5d114
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-18 08:47:37 +0530

    Time: 6 ms (46.43%), Space: 44.8 MB (34.09%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+    int countup = 0, countleft = 0;
+
+    for(int i = 0; i < moves.length(); i++){
+        char chr = moves.charAt(i);
+
+        if(chr == 'U') countup++;
+        else if(chr == 'D') countup--;
+        else if(chr == 'L') countleft++;
+        else countleft--;
+        
+    }
+
+    return countup== 0 && countleft == 0;
+    }
+}

ec11f5acf751312aa05cce751c070da27c0576b1
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-20 09:41:11 +0530

    Time: 18 ms (12.01%), Space: 45.8 MB (41.96%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -3,22 +3,20 @@ class Solution {
         HashSet<Integer> hs = new HashSet<>();  
 
         Arrays.sort(nums);
-         
+
         int missing = 1,twotime = 0;
         int[] res = new int[2];
 
         for(int num:nums){
             if(num == missing) missing++;
+            if(hs.contains(num)) res[0] = num;
+            else
+            hs.add(num);
         }
         res[1] = missing;
 
         for(int num:nums){
-            if(hs.contains(num)){
-                res[0] = num;
-                break;
-            }
-            else
-            hs.add(num);
+            
         }
 
     return res;

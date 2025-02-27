09a44c58970ed853ad4d1f10f340884eff5ad082
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-02-27 15:45:37 +0530

    Time: 86 ms (19.09%), Space: 57.3 MB (16.48%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,37 +1,15 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length <= 1) return nums;
-        mergeSort(nums, 0, nums.length - 1);
-        return nums;
-    }
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
 
-    private void mergeSort(int[] nums, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-
-            mergeSort(nums, left, mid);
-            mergeSort(nums, mid + 1, right);
-            merge(nums, left, mid, right);
+        for(int i:nums){
+            pq.add(i);
         }
-    }
-
-    private void merge(int[] nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] leftArr = new int[n1];
-        int[] rightArr = new int[n2];
-
-        for (int i = 0; i < n1; i++) leftArr[i] = nums[left + i];
-        for (int i = 0; i < n2; i++) rightArr[i] = nums[mid + 1 + i];
-
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (leftArr[i] <= rightArr[j]) nums[k++] = leftArr[i++];
-            else nums[k++] = rightArr[j++];
+        int[] result = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            result[i] = pq.poll();
         }
-
-        while (i < n1) nums[k++] = leftArr[i++];
-        while (j < n2) nums[k++] = rightArr[j++];
+        return result;
     }
 }

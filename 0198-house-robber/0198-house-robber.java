a5059fb935055330e1d5704744f85321d758429d
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-03-15 06:49:58 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,5 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 0) return 0;
-
         int prev = 0,prev1 =0;
 
         for(int num:nums){

f407fc4e5a88c30e3876547aee9ac6be6e8eb6ad
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-06-24 23:20:50 +0530

    Time: 1 ms (99.64%), Space: 58.3 MB (6.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,7 +3,7 @@ class Solution {
         int totalProduct = 1;
         int zeroCount = 0;
 
-        // First pass: Calculate total product (excluding zeros) and count zeros
+ 
         for (int num : nums) {
             if (num != 0) {
                 totalProduct *= num;
@@ -12,10 +12,9 @@ class Solution {
             }
         }
 
-        // Second pass: Build the result
         for (int i = 0; i < nums.length; i++) {
             if (zeroCount > 1) {
-                nums[i] = 0; // More than one zero, all products will be zero
+                nums[i] = 0;
             } else if (zeroCount == 1) {
                 nums[i] = (nums[i] == 0) ? totalProduct : 0;
             } else {

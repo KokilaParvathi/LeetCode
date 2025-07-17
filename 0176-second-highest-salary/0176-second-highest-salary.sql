357510b42dda0068ee2ef7c22453b5e1e23e4028
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-07-17 06:26:47 +0530

    Time: 291 ms (29.32%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,6 @@
-SELECT
-    (SELECT DISTINCT salary
-     FROM Employee
-     ORDER BY salary DESC
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;

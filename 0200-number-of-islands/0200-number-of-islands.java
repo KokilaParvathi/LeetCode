28a6b78a6064033553a84ca1aa286627e71ef9b7
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-08-20 07:03:54 +0530

    Time: 3 ms (83.05%), Space: 49.9 MB (40.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        // Code here
         int m = grid.length;
         int n = grid[0].length;
-        int islands = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j,m,n);
-                     islands++;
-                } 
+        int island = 0;
+        
+        for(int i = 0 ; i < m; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == '1'){
+                    ch(grid,i,j,m,n);
+                    island++;
+                }
             }
-        }  
-        return islands;
+        }
+        
+        return island;
     }
-    
-    public void dfs(char[][] grid, int i, int j, int m, int n){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] != '1') return;
-        grid[i][j]='$';
-
-        dfs(grid,i+1,j,m,n); //up
-        dfs(grid,i-1,j,m,n); //down
-        dfs(grid,i,j-1,m,n); //left
-        dfs(grid,i,j+1,m,n); //right
+    void ch(char[][] grid,int i,int j,int m,int n){
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') return;
+        grid[i][j] = '*';
+       ch(grid,i+1,j,m,n);
+       ch(grid,i-1,j,m,n);
+       ch(grid,i,j+1,m,n);
+       ch(grid,i,j-1,m,n);
+        
     }
 }

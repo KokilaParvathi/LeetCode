fee92ac43b577857ed975d19e7082007fbaa67a3
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-08-21 09:34:20 +0530

    Time: 0 ms (100%), Space: 42.4 MB (72.92%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,6 +6,6 @@ class Solution {
         if(nums[itr-2] < nums[itr-1] && nums[itr-1] > nums[itr]) return itr-1;
     }
     
-    return nums[0]>nums[1]?0:nums.length-1; 
+    return nums[nums.length-2] > nums[nums.length-1]?0:nums.length-1; 
     }
 }

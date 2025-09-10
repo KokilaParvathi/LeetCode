d8da62c94aeab0cec48eb0e8fdde33275d46cf13
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-09-11 00:07:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.46%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,11 @@
+int addDigits(int num) {
+    int sum = 0;
+
+    while(num > 0){
+        sum+=(num%10);
+        num/=10;
+    }
+
+    if(sum > 9) return addDigits(sum);
+    else return sum;
+}

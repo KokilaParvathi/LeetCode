ecb2136a8432946467153bc5af9c49ec9f0302e8
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-09-12 02:27:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (45.63%) - LeetHub

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean findRotation(int[][] matrix, int[][] target) {
+        for (int k = 0; k < 4; k++) {
+            if (isEqual(matrix, target))
+                return true;
+            rotate(matrix);
+        }
+        return false;
+    }
+
+    private void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    private boolean isEqual(int[][] a, int[][] b) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] != b[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}

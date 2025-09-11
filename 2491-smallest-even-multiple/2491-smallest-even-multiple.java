9c1142af95d78d87d5c48b9b908cec6f86437344
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-09-11 22:19:50 +0530

    Time: 0 ms (100%), Space: 40.6 MB (35.01%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,10 +1,9 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-    int i = n;
-    while((i % n == 0) != (i % 2 == 0)){
-        i+=n;
-    }    
-
-    return i;
+        int i = n;
+        while ((i % n == 0) != (i % 2 == 0)) {
+            i += n;
+        }
+        return i;
     }
 }

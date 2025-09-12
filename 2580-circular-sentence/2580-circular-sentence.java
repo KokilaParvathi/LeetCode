3f4fac8014de3e13fa72968ec7d8b035b76e508d
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-09-12 11:38:14 +0530

    Time: 1 ms (96.25%), Space: 41.8 MB (97.63%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isCircularSentence(String s) {
+        if (s.charAt(0) != s.charAt(s.length() - 1))
+            return false;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i + 1) == ' ') {
+                if (s.charAt(i) != s.charAt(i + 2))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}

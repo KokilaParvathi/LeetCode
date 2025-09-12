64a6848064630c9900721264d936d94ced7fceab
Kokila P <134096094+KokilaParvathi@users.noreply.github.com>
2025-09-12 11:44:14 +0530

    Time: 744 ms (15.53%), Space: 41.5 MB (74.51%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean ispali(int s,int e,String st){
+
+        while(s <= e){ 
+            if(st.charAt(s) != st.charAt(e))
+                return false;
+            s++;
+            e--;
+        }
+
+        return true;
+    }
+
+    public int countSubstrings(String s) {
+        int count = 0;
+
+        for(int  i = 0; i < s.length(); i++){
+            for(int j = i; j <s.length(); j++){
+                if(ispali(i,j,s)) count++;
+            }
+        }
+        
+        return count;
+    }
+}
